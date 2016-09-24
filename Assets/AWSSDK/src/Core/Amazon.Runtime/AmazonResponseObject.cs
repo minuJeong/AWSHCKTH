e9f9cbe9a316f44f28ee2d76a68bd9af96ccceff
Minu Jeong <mjung@ea.com>
2016-09-24 14:00:14 +0900

    add fb sdk, was sdk (not working)

--- Assets/AWSSDK/src/Core/Amazon.Runtime/AmazonResponseObject.cs
+++ Assets/AWSSDK/src/Core/Amazon.Runtime/AmazonResponseObject.cs
@@ -21,26 +21,45 @@ using System.Text;
 
 namespace Amazon.Runtime
 {
-    public delegate void AmazonServiceCallback<TRequest, TResponse>(AmazonServiceResult<TRequest, TResponse> responseObject)
+	public delegate void AmazonServiceCallback<TRequest, TResponse> (AmazonServiceResult<TRequest, TResponse> responseObject)
         where TRequest : AmazonWebServiceRequest
         where TResponse : AmazonWebServiceResponse;
 
-    public class AmazonServiceResult<TRequest, TResponse>
+	public class AmazonServiceResult<TRequest, TResponse>
         where TRequest : AmazonWebServiceRequest
         where TResponse : AmazonWebServiceResponse
-    {
-        public TRequest Request { get; internal set; }
-        public TResponse Response { get; internal set; }
-        public Exception Exception { get; internal set; }
-        public object state { get; internal set; }
+	{
+		public TRequest Request
+		{
+			get;
+			internal set;
+		}
 
-	public AmazonServiceResult(TRequest request, TResponse response, Exception exception, object state)
-        {
-            this.Request = request;
-            this.Response = response;
-            this.Exception = exception;
-            this.state = state;
-        }
-    }
+		public TResponse Response
+		{
+			get;
+			internal set;
+		}
+
+		public Exception Exception
+		{
+			get;
+			internal set;
+		}
+
+		public object state
+		{
+			get;
+			internal set;
+		}
+
+		public AmazonServiceResult (TRequest request, TResponse response, Exception exception, object state)
+		{
+			this.Request = request;
+			this.Response = response;
+			this.Exception = exception;
+			this.state = state;
+		}
+	}
 
 }
